fc37bd59a20d3116a5659f38cf69639407916dfc
slonob0y <zazhil24@gmail.com>
2021-09-26 08:01:14 +0700

    modified folder

--- routes/routes.go
+++ routes/routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/slonob0y/qms/controllers"
 	"github.com/gofiber/fiber/v2"
+	"github.com/slonob0y/qms/handler"
 )
 
 func Setup(app *fiber.App) {
 
-	app.Post("/api/register", controllers.Register)
-	app.Post("/api/login", controllers.Login)
-	app.Get("/api/user", controllers.User)
-	app.Post("/api/logout", controllers.Logout)
+	app.Post("/api/register", handler.Register)
+	app.Post("/api/login", handler.Login)
+	app.Get("/api/user", handler.User)
+	app.Post("/api/logout", handler.Logout)
 
 }
