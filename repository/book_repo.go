0f4ccbfaf2e3809f7758cd1445bf64bc8a8e18f9
slonob0y <zazhil24@gmail.com>
2021-10-05 21:20:32 +0700

    mod: add 2 endpoints

--- repository/book_repo.go
+++ repository/book_repo.go
@@ -18,6 +18,8 @@ func NewBookRepository(db *gorm.DB) *BookRepository {
 type BookRepoInterface interface {
 	CreateBook(book models.SlotBooking) (models.SlotBooking, error)
 	FindAllBank() ([]models.Bank, error)
+	DeleteBook(status string) error
+	GetBankById(id string) (models.Bank, error)
 }
 
 func(r *BookRepository) CreateBook(book models.SlotBooking) (models.SlotBooking, error) {
@@ -31,4 +33,24 @@ func(r *BookRepository) FindAllBank() ([]models.Bank, error) {
 	findResult := r.db.Find(&banks)
 
 	return banks, findResult.Error
+}
+
+func(r *BookRepository) DeleteBook(status string) error {
+	var book models.SlotBooking
+	findResult := r.db.Unscoped().Where("status = ?", status).Delete(&book)
+	if findResult.Error != nil {
+		return findResult.Error
+	}
+
+	if findResult.RowsAffected == 0 {
+		return gorm.ErrRecordNotFound
+	}
+	
+	return nil
+}
+
+func(r *BookRepository) GetBankById(id string) (models.Bank, error) {
+	var bank models.Bank
+	findResult := r.db.Where("id = ?", id).First(&bank)
+	return bank, findResult.Error
 }
