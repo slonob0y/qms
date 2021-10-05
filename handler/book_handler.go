867e426530d476f666a3f5c2df5d437204af1271
slonob0y <zazhil24@gmail.com>
2021-10-05 21:20:04 +0700

    mod: add 2 endpoints

--- handler/book_handler.go
+++ handler/book_handler.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"errors"
+
 	"github.com/gofiber/fiber/v2"
 	"github.com/slonob0y/qms/models"
 	"github.com/slonob0y/qms/service"
+	"gorm.io/gorm"
 )
 
 type BookHandler struct {
@@ -19,6 +22,8 @@ func NewBookHandler(bookService service.BookServiceInterface) *BookHandler {
 type BookHandlerInterface interface {
 	CreateBook(c *fiber.Ctx) error
 	GetBank(c *fiber.Ctx) error
+	DeleteBook(c *fiber.Ctx) error
+	GetBankById(c *fiber.Ctx) error
 }
 
 func (h *BookHandler) CreateBook(c *fiber.Ctx) error {
@@ -55,4 +60,47 @@ func(h *BookHandler) GetBank(c *fiber.Ctx) error {
 	}
 
 	return c.JSON(banks)
+}
+
+func(h *BookHandler) DeleteBook(c *fiber.Ctx) error {
+	status := c.Params("status")
+
+	err := h.bookService.DeleteBook(status)
+	if err != nil {
+		var statusCode = fiber.StatusInternalServerError
+
+		if errors.Is(err, gorm.ErrRecordNotFound) {
+			statusCode = fiber.StatusNotFound
+		}
+
+		return c.Status(statusCode).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error":  false,
+		"result": "success delete data",
+	})
+	
+}
+
+func(h *BookHandler) GetBankById(c *fiber.Ctx) error {
+	id := c.Params("id")
+
+	response, err := h.bookService.GetBankById(id)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).JSON(fiber.Map{
+			"error": true,
+			"msg":   err.Error(),
+		})
+	}
+
+	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+		"error":  false,
+		// "msg":    "success update data",
+		"result": response,
+	})
+	
 }
